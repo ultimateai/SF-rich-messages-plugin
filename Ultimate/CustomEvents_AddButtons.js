3d0a0d8ecbbdb6e774cedc04175d6484a43b0e61
RiezanovRoman <rriezanov@redrocket.software>
2022-12-15 12:52:22 +0200

    some small fixes

--- Ultimate/CustomEvents_AddButtons.js
+++ Ultimate/CustomEvents_AddButtons.js
@@ -11,7 +11,7 @@
   link.setAttribute("type", "text/css");
   link.setAttribute(
     "href",
-    "https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&family=Roboto+Mono&display=swap"
+    "https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap"
   );
   document.head.appendChild(link);
 
